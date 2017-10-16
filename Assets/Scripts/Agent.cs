04ea87a28246a753fdc1da82d316af8903a931d8
Hu Kaidong <hukaidonghkd@gmail.com>
2017-10-16 19:08:54 -0400

    .

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    Animator anim;
+
+    public Vector3 desiredVelo = Vector3.zero;
+    Vector3 currentVelo = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentVelo = Vector3.Lerp(currentVelo, desiredVelo, Time.deltaTime);
+        if (currentVelo.magnitude > 0.1)
+        {
+            anim.SetBool("move", true);
+            anim.SetFloat("velx", currentVelo.x);
+            anim.SetFloat("vely", currentVelo.z);
+        }
+        else
+        {
+            anim.SetBool("move", false);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,62 @@
+﻿#define DVORAK
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float desiredSpeed = 1.0F;
+    public GameObject skeleton;
+#if DVORAK
+    enum KEYBOARD_INPUT : int {
+		LEFT = KeyCode.A,
+		RIGHT = KeyCode.E,
+		FORWARD = KeyCode.Comma,
+		BACKWARD = KeyCode.O,
+        ACCEL = KeyCode.LeftShift,
+    }
+#else
+    enum KEYBOARD_INPUT : int
+    {
+        LEFT = KeyCode.A,
+        RIGHT = KeyCode.D,
+        FORWARD = KeyCode.W,
+        BACKWARD = KeyCode.S,
+        ACCEL = KeyCode.LeftShift,
+    }
+#endif
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = Vector3.zero;
+        float mvMulIndex = 1;
+        if (Input.GetKey((KeyCode)KEYBOARD_INPUT.RIGHT))
+        {
+            movement.x += 1;
+        }
+        if (Input.GetKey((KeyCode)KEYBOARD_INPUT.LEFT))
+        {
+            movement.x -= 1;
+        }
+        if (Input.GetKey((KeyCode)KEYBOARD_INPUT.FORWARD))
+        {
+            movement.z += 1;
+        }
+        if (Input.GetKey((KeyCode)KEYBOARD_INPUT.BACKWARD))
+        {
+            movement.z -= 1;
+        }
+        if (Input.GetKey((KeyCode)KEYBOARD_INPUT.ACCEL))
+        {
+            mvMulIndex = 2;
+        }
+        movement = movement.normalized * desiredSpeed * mvMulIndex;
+        skeleton.GetComponent<Agent>().desiredVelo = movement;
+    }
+}
